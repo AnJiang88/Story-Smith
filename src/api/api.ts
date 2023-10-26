3e63ee6c7aabab0b71dbcf437b985badd6219afd
Clay Poffenberger <claypoff@gmail.com>
2023-10-26 09:30:37 -0700

    Add ability to toggle enabling auto feedback

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,17 +6,20 @@ export async function textCompletion(prompt: string, maxTokens=512) {
   return getTextCompletion(input, maxTokens);
 }
 
-const systemPrimer = "You are a helpful assistant for a student writing an essay. Provide constructive pointers and suggestions to help the student improve their writing. Focus on specific details, structure, and clarity. Avoid writing full sentences."
-
+const systemPrimer = "You are my helpful assistant for writing a creative story. Read my creative writing and analyze it to see how it answers the prompt. Provide me constructive suggestions to help me improve my writing. Focus on giving me specific details, structure, and clarity. Give your response in short senteces and don't give me more than 40 words."
 
 export async function chatCompletion(prompt: string, draft: string, maxTokens=512) {
   const systemMessage = {
     role: 'system',
     content: systemPrimer + prompt
   }
+  const promptMessage = {
+    role: 'user',
+    content: "This is my prompt: " + prompt
+  }
   const userMessage = {
     role: 'user',
-    content: draft
+    content: "Here is my draft:\n\n" + draft
   }
-  return getChatCompletion([systemMessage, userMessage], maxTokens);
+  return getChatCompletion([systemMessage, promptMessage, userMessage], maxTokens);
 }
